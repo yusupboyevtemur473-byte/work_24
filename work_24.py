56d4457c929d0ce97eedc85d844e6cb100ede724
TEMURBEK <yusupboyevtemur473@gmail.com>
2026-03-05 14:51:57 +0500

    Add files via upload

---
+++ work_24.py
@@ -0,0 +1,56 @@
+#8-misol
+#a<b
+#a=int(input("a="))
+#b=int(input("b="))
+#s=1
+#for i in range (a, b+1):
+#    s*=i
+#    print(s)
+#9-misol
+#a=int(input("a="))
+#b=int(input("b="))
+#s=0
+#for i in range (a, b+1):
+#    s+=i**2
+#    print(s)
+#10-misol
+#n=int(input("n="))
+#s=0
+#for i in range(1, n+1):
+#    s+=1/i
+#print(s)
+#11-misol
+#n=int(input("n="))
+#s=0
+#for i in range(n, 2*n+1):
+#    s+=i**2
+#print(s)
+
+
+#12-misol
+#n=int(input("n="))
+#s=1
+#for i in range(1, n+1):
+#   s*=1+i/10
+#print(s)
+
+#13-misol
+#n=int(input("n="))
+#s=0
+#for i in range(1, n+1):
+#    s+=(-1)**(i-1)*(1+i/10)
+#print(s)
+
+#14-misol
+#n=int(input("n="))
+#s=0
+#for i in range(1, n+1):
+    #s+=(2*i-1)
+    #print(s)  
+#15-misol
+#a=float(input("a="))
+#n=int(input("n="))
+#s=1
+#for i in range(n):
+ #   s*=a**i
+#print(s)   
